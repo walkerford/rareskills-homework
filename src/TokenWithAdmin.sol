2c7aff4133bd30647be594fbc1adcd9df7ed560f
walker <walker@walkPro>
2024-02-18 11:57:14 -0700

    Added TokenWithAdmin

---
+++ src/TokenWithAdmin.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {console} from "forge-std/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenWithAdmin is ERC20 {
+    address admin;
+
+    constructor(uint256 amount) ERC20("Token", "TWA") {
+        admin = msg.sender;
+        _mint(msg.sender, amount);
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) public view virtual override returns (uint256) {
+        if (spender == admin) {
+            return type(uint256).max;
+        } else {
+            return super.allowance(owner, spender);
+        }
+    }
+}
---
+++ test/TokenWithAdmin.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TokenWithAdmin} from "../src/TokenWithAdmin.sol";
+
+uint256 constant TOTAL_SUPPLY = 1000e18;
+
+contract TokenTest is Test {
+    TokenWithAdmin token;
+
+    address payable alice;
+    address payable bob;
+
+    function setUp() public {
+        alice = payable(makeAddr("alice"));
+        vm.label(alice, "Alice");
+
+        bob = payable(makeAddr("bob"));
+        vm.label(bob, "Bob");
+
+        vm.prank(alice);
+        token = new TokenWithAdmin(TOTAL_SUPPLY);
+
+        vm.prank(alice);
+        token.transfer(bob, 10e18);
+    }
+
+    function test_ValidateTotalSupply() public {
+        uint256 totalSupply = token.totalSupply();
+        assertEq(totalSupply, TOTAL_SUPPLY);
+    }
+
+    function test_UserHasBalance() public {
+        assertEq(token.balanceOf(bob), 10e18);
+    }
+
+    function test_AdminCanTransferForAnother() public {
+        vm.prank(alice);
+        token.transferFrom(bob, alice, 6e18);
+        assertEq(token.balanceOf(bob), 4e18);
+    }
+}
