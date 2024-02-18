fc59903e9ec5fbb954a56bd3d2eb1e2a87ccd9b6
walker <walker@walkPro>
2024-02-18 11:57:23 -0700

    Added TokenWithSanctions

---
+++ src/TokenWithSanctions.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {console} from "forge-std/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @dev Indicates a sanctioned `receiver`. Used in transfers.
+ * @param receiver Address to which tokens are being transferred.
+ */
+error ERC20SanctionedReceiver(address receiver);
+
+/**
+ * @dev Indicates a sanctioned `sender`. Used in transfers.
+ * @param sender Address to which tokens are being transferred.
+ */
+error ERC20SanctionedSender(address sender);
+
+/**
+ * @dev Indicates an address that is not allowed to perform sanctioner functions.
+ * @param address_ Address attempting sanctioner function.
+ */
+error ERC20UnauthorizedSanctioner(address address_);
+
+contract TokenWithSanctions is ERC20 {
+    address sanctioner;
+    mapping(address => bool) private _sanctioned;
+
+    constructor(uint256 amount) ERC20("Token", "TWS") {
+        sanctioner = msg.sender;
+        _mint(msg.sender, amount);
+    }
+
+    function ban(address sanctioned) public {
+        if (msg.sender != sanctioner) {
+            revert ERC20UnauthorizedSanctioner(msg.sender);
+        }
+        _sanctioned[sanctioned] = true;
+    }
+
+    function unban(address sanctioned) public {
+        if (msg.sender != sanctioner) {
+            revert ERC20UnauthorizedSanctioner(msg.sender);
+        }
+        _sanctioned[sanctioned] = false;
+    }
+
+    function isSanctioned(address sanctioned) public view returns (bool) {
+        return _sanctioned[sanctioned];
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override {
+        if (_sanctioned[from]) {
+            revert ERC20SanctionedSender(from);
+        } else if (_sanctioned[to]) {
+            revert ERC20SanctionedReceiver(to);
+        }
+        super._update(from, to, value);
+    }
+}
---
+++ test/TokenWithSanctions.t.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TokenWithSanctions, ERC20SanctionedReceiver, ERC20SanctionedSender, ERC20UnauthorizedSanctioner} from "../src/TokenWithSanctions.sol";
+
+uint256 constant TOTAL_SUPPLY = 1000e18;
+
+contract TokenWithSanctionsTest is Test {
+    TokenWithSanctions token;
+
+    address payable alice;
+    address payable bob;
+
+    function setUp() public {
+        alice = payable(makeAddr("alice"));
+        vm.label(alice, "Alice");
+
+        bob = payable(makeAddr("bob"));
+        vm.label(bob, "Bob");
+
+        vm.prank(alice);
+        token = new TokenWithSanctions(TOTAL_SUPPLY);
+
+        vm.prank(alice);
+        token.transfer(bob, 10e18);
+    }
+
+    function test_ValidateTotalSupply() public {
+        uint256 totalSupply = token.totalSupply();
+        assertEq(totalSupply, TOTAL_SUPPLY);
+    }
+
+    function test_UserHasBalance() public {
+        assertEq(token.balanceOf(bob), 10e18);
+    }
+
+    function test_RevertWhen_NonAdminTriesBanning() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ERC20UnauthorizedSanctioner.selector,
+                address(this)
+            )
+        );
+        token.ban(bob);
+    }
+
+    function test_RevertWhen_SendingToSanctioned() public {
+        // Admin bans bob
+        vm.prank(alice);
+        token.ban(bob);
+
+        // Attempt to send to bob fails
+        vm.expectRevert(
+            abi.encodeWithSelector(ERC20SanctionedReceiver.selector, bob)
+        );
+        vm.prank(alice);
+        token.transfer(bob, 10e18);
+    }
+
+    function test_RevertWhen_SendingFromSanctioned() public {
+        // Admin bans bob
+        vm.prank(alice);
+        token.ban(bob);
+
+        // Attempt to send from bob fails
+        vm.expectRevert(
+            abi.encodeWithSelector(ERC20SanctionedSender.selector, bob)
+        );
+        vm.prank(bob);
+        token.transfer(alice, 5e18);
+    }
+}
