4ad02e136b244886c14d1c59db584a7db322a477
walker <walker@walkPro>
2024-02-22 13:23:53 -0700

    WIP TokenWithBonding

---
+++ src/TokenWithBonding.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {console} from "forge-std/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+uint256 constant MULT = 1e15;
+
+error TokenInsufficientFunding(uint256 amount);
+
+contract TokenWithBonding is ERC20 {
+    constructor(uint256 amount) ERC20("Token", "TWB") {
+        _mint(msg.sender, amount);
+    }
+
+    function purchase(uint256 amount) external payable {
+        console.log("purchase() amount:", amount / MULT);
+        uint256 price_ = this.price(amount);
+        console.log("price:", price_ / MULT);
+        console.log("msg.value", msg.value / MULT);
+        if (msg.value < price_) {
+            revert TokenInsufficientFunding(price_ / MULT);
+        }
+
+        _mint(msg.sender, amount);
+    }
+
+    function price(uint256 amount) external view returns (uint256) {
+        uint256 totalSupply = this.totalSupply();
+        console.log("price() totalSupply:", totalSupply / MULT);
+        uint256 price_ = ((((totalSupply + amount) ** 2)) -
+            (totalSupply ** 2)) / 2e18;
+        return price_;
+    }
+}
---
+++ test/TokenWithBonding.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TokenWithBonding} from "../src/TokenWithBonding.sol";
+
+contract TokenWithBondingTest is Test {
+    TokenWithBonding token;
+
+    address payable alice;
+    address payable bob;
+
+    function setUp() public {
+        alice = payable(makeAddr("alice"));
+        vm.label(alice, "Alice");
+        vm.deal(alice, 10 ether);
+
+        bob = payable(makeAddr("bob"));
+        vm.label(bob, "Bob");
+
+        vm.prank(alice);
+        token = new TokenWithBonding(0);
+    }
+
+    function test_LinearPurchase() public {
+        vm.prank(alice);
+        token.purchase{value: 1 ether / 2}(1e18);
+        assertEq(token.balanceOf(alice), 1e18);
+
+        assertEq(token.totalSupply(), 1e18);
+    }
+}
