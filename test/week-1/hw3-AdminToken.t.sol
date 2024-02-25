b67dee1fd3e2abdba93035b491558389e98b8c6c
walker <walker@walkPro>
2024-02-24 19:55:25 -0700

    Adding other week-2 files

--- test/week-1/hw3-AdminToken.t.sol
+++ test/week-1/hw3-AdminToken.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
 import {AdminToken} from "../../src/week-1/hw3-AdminToken.sol";
