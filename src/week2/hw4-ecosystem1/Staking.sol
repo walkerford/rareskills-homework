6df7319e6fe9e3d99a489af56f6cff8f445e6a21
walker <walker@walkPro>
2024-02-28 02:28:48 -0700

    Ecosystem1 is working, rewards can be withdrawn

--- src/week2/hw4-ecosystem1/Staking.sol
+++ src/week2/hw4-ecosystem1/Staking.sol
@@ -6,65 +6,90 @@ import "forge-std/console.sol";
 import "./RewardToken.sol";
 import "./LimitedNFT.sol";
 
-error UnauthorizedUnstake();
+error NotAuthorized();
+error NoStake();
+error NotEnoughCredits();
 
 uint256 constant UNLOCK_PERIOD_IN_BLOCKS = 10;
 uint256 constant SECONDS_PER_BLOCK = 12;
 uint256 constant SECONDS_PER_PERIOD = 60 * 60 * 24;
 uint256 constant BLOCKS_PER_PERIOD = SECONDS_PER_PERIOD / SECONDS_PER_BLOCK;
 uint256 constant REWARDS_PER_PERIOD = 10 ether;
-uint256 constant REWARDS_PER_BLOCK = REWARDS_PER_PERIOD / BLOCKS_PER_PERIOD;
-
 
 contract Staking is IERC721Receiver {
     struct StakedNft {
         address owner;
-        uint256 lastPaidBlock;
+        uint256 lastUnpaidBlock;
+        uint256 credits;
     }
-    
+
     /// @notice RewardToken contract
-    RewardToken rewardToken;
+    RewardToken public rewardToken;
 
     /// @notice Nft contract
     LimitedNFT nft;
 
     /// @notice State of each staked NFT
-    mapping(uint256 => StakedNft) stakedNfts;
+    mapping(uint256 tokenId => StakedNft) stakedNfts;
 
     constructor(LimitedNFT nft_) {
         rewardToken = new RewardToken();
         nft = nft_;
+        nft.setApprovalForAll(address(this), true);
     }
 
     function stake(uint256 tokenId) external {
         // Q: Should this be memory or storage?
-        StakedNft memory stakedNft = StakedNft({owner: msg.sender, lastPaidBlock: block.number});
+        StakedNft memory stakedNft = StakedNft({
+            owner: msg.sender,
+            lastUnpaidBlock: block.number,
+            credits: 0
+        });
         stakedNfts[tokenId] = stakedNft;
         nft.safeTransferFrom(msg.sender, address(this), tokenId);
     }
 
     function unstake(uint256 tokenId) external {
         // Validate sender is owner
-        if (stakedNfts[tokenId].owner != msg.sender) revert UnauthorizedUnstake();
+        if (stakedNfts[tokenId].owner != msg.sender) revert NotAuthorized();
 
-        nft.approve(address(this), tokenId);
+        _updateCredits(tokenId);
+        stakedNfts[tokenId].lastUnpaidBlock = 0;
+
         nft.safeTransferFrom(address(this), msg.sender, tokenId);
     }
 
-    function withdraw() external {
-
+    function withdraw(uint256 tokenId) external {
+        _updateCredits(tokenId);
+        _withdraw(tokenId, stakedNfts[tokenId].credits);
     }
 
-    /// @notice Check the balance of rewards yet to be withdrawn.  Due to unlock period,
+    function withdraw(uint256 tokenId, uint256 amount) external {
+        _updateCredits(tokenId);
+        _withdraw(tokenId, amount);
+    }
+
+    function _withdraw(uint256 tokenId, uint256 amount) private {
+        StakedNft storage staked = stakedNfts[tokenId];
+
+        // Validate owner
+        if (staked.owner != msg.sender) revert NotAuthorized();
+
+        // Validate amount
+        if (amount > staked.credits) revert NotEnoughCredits();
+
+        // Reduce credits
+        staked.credits -= amount;
+
+        // Mint rewards
+        rewardToken.mint(staked.owner, amount);
+    }
+
+    /// @notice Shows the balance of rewards yet to be withdrawn.  Due to unlock period,
     /// there may be more rewards accrued than are avaiable to withdraw.
-    function rewardsBalanceOf(uint256 tokenId) external view returns(uint256) {
-        uint256 blocks;
-        unchecked {
-            // lastPaidBlock is updated by contract logic and block numbers only advance
-            blocks = block.number - stakedNfts[tokenId].lastPaidBlock;
-        }
-        uint256 rewards = blocks * REWARDS_PER_BLOCK;
-        return rewards;
+    function credits(uint256 tokenId) external returns (uint256) {
+        _updateCredits(tokenId);
+        return stakedNfts[tokenId].credits;
     }
 
     function onERC721Received(
@@ -74,5 +99,33 @@ contract Staking is IERC721Receiver {
         bytes calldata
     ) external pure returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function _updateCredits(uint256 tokenId) private {
+        StakedNft storage staked = stakedNfts[tokenId];
+
+        // Return if not staked
+        // A non-zero `lastUnpaidBlock` indicates that the nft is still actively staked
+        if (staked.lastUnpaidBlock == 0) return;
+
+        // Get blocks since last payment
+        uint256 blocks = block.number - staked.lastUnpaidBlock;
+
+        // Rewards are only released in groups by period
+        if (blocks >= BLOCKS_PER_PERIOD) {
+            // Get how many full periods have passed
+            uint256 periods = blocks / BLOCKS_PER_PERIOD;
+
+            // Compute rewards across all periods
+            uint256 rewards = periods * REWARDS_PER_PERIOD;
+
+            // Accumulate the rewards
+            staked.credits += rewards;
+
+            // Update the last paid block
+            staked.lastUnpaidBlock =
+                staked.lastUnpaidBlock +
+                (periods * BLOCKS_PER_PERIOD);
+        }
     }
 }
--- test/week2/hw4-ecosystem1/Staking.t.sol
+++ test/week2/hw4-ecosystem1/Staking.t.sol
@@ -20,9 +20,11 @@ contract TestStaking is Test {
         alice = makeAddr("alice");
         vm.deal(alice, 2 ether);
 
+        // Alice gets first nft
         vm.prank(alice);
         tokenId1 = nft.mint{value: 1 ether}();
 
+        // Alice gets second nft
         vm.prank(alice);
         tokenId2 = nft.mint{value: 1 ether}();
     }
@@ -39,7 +41,7 @@ contract TestStaking is Test {
         staking.unstake(tokenId);
     }
 
-    function test_Staking() external {
+    function test_StakingAndUnstaking() external {
         assertEq(nft.balanceOf(alice), 2);
 
         _stake(alice, tokenId1);
@@ -53,7 +55,6 @@ contract TestStaking is Test {
     }
 
     function test_RevertWhen_StakingUnauthorized() external {
-
         vm.expectRevert(
             abi.encodeWithSelector(
                 IERC721Errors.ERC721InsufficientApproval.selector,
@@ -64,16 +65,58 @@ contract TestStaking is Test {
         vm.prank(alice);
         staking.stake(tokenId2);
     }
-    
-    function test_RewardsBalanceOf() external {
+
+    function test_RewardsAccumulation() external {
         // Stake happens in block 1
         _stake(alice, tokenId1);
-        
-        // Advance blockchain to block 2
-        vm.roll(2);
 
-        // Test balance after one block
-        uint256 rewards = staking.rewardsBalanceOf(tokenId1);
-        assertEq(rewards, REWARDS_PER_BLOCK);
+        // Advance a full period
+        vm.roll(BLOCKS_PER_PERIOD + 1);
+
+        // Test rewards balance
+        uint256 credits = staking.credits(tokenId1);
+        assertEq(credits, REWARDS_PER_PERIOD);
+
+        // Withdraw rewards
+        vm.prank(alice);
+        staking.withdraw(tokenId1);
+        assertEq(staking.rewardToken().balanceOf(alice), REWARDS_PER_PERIOD);
+
+        // Should have no credits left
+        credits = staking.credits(tokenId1);
+        assertEq(credits, 0);
+
+        // Advance partial period
+        vm.roll((BLOCKS_PER_PERIOD * 2) - 10);
+
+        // Should have no credits
+        credits = staking.credits(tokenId1);
+        assertEq(credits, 0);
+
+        // Advance another period
+        vm.roll((BLOCKS_PER_PERIOD * 2) + 1);
+
+        // Should have another one period's worth of credits
+        credits = staking.credits(tokenId1);
+        assertEq(credits, REWARDS_PER_PERIOD);
+
+        // Unstake
+        vm.prank(alice);
+        staking.unstake(tokenId1);
+
+        // Advance another period
+        vm.roll((BLOCKS_PER_PERIOD * 2) + 1);
+
+        // Should not have accumulated more credits
+        credits = staking.credits(tokenId1);
+        assertEq(credits, REWARDS_PER_PERIOD);
+
+        // Can still withdraw, even after unstaking
+        vm.prank(alice);
+        staking.withdraw(tokenId1);
+        assertEq(
+            staking.rewardToken().balanceOf(alice),
+            REWARDS_PER_PERIOD * 2
+        );
     }
 }
