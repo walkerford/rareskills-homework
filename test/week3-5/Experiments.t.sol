438952db3b084d81f0c7827938c0390fc3700b28
walker <walker@walkPro>
2024-03-01 22:28:10 -0700

    Added some experiments

---
+++ test/week3-5/Experiments.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import "forge-std/Test.sol";
+import "../../src/week3-5/Experiments.sol";
+
+contract TestExperiments is Test {
+    Experiments ex;
+
+    function setUp() external {
+        ex = new Experiments();
+    }
+
+    function test_showUint112() external view {
+        console.log("uint112.max", type(uint112).max);
+        console.log("2**112", 2 ** 112);
+        int16 a = -1;
+        bytes memory b = abi.encodePacked(a);
+        console.logBytes(b);
+    }
+}
