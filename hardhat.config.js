86e2122a0905d77a3f04d1e8a3a62f84f31429da
walker <walker@walkPro>
2024-04-06 13:23:43 -0600

    Added testing framework to Overmint1-ERC1155

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,16 +3,19 @@ require("hardhat-tracer");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.15",
-            },
-        ],
-        overrides: {
-            "contracts/NameServiceBank.sol": {
-                version: "0.7.0",
-            },
-        },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.15",
+      },
+    ],
+    overrides: {
+      "contracts/NameServiceBank.sol": {
+        version: "0.7.0",
+      },
     },
+  },
+  paths: {
+    sources: "./src/week8-9-security/Overmint1-ERC1155",
+  },
 };
