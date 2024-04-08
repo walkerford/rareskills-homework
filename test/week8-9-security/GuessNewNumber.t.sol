88c4c6b687dba378427ae2e461c23e5817e4b753
Walker Ford <walker.ford@protocol.ai>
2024-04-08 12:04:23 -0600

    Refactored GuessNewNumber

--- test/week8-9-security/GuessNewNumber.t.sol
+++ test/week8-9-security/GuessNewNumber.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "../src/GuessNewNumber.sol";
+import "week8-9-security/GuessNewNumber.sol";
 
 contract GuessNewNumberTest is Test {
     GuessNewNumber public guessNewNumber;
