a86352c5e0100d102135b3d989497244423413e2
walker <walker@walkPro>
2024-04-11 16:40:16 -0600

    Added SideEntranceLenderPool

---
+++ test/week8-9-security/SideEntranceLenderPool.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "week8-9-security/SideEntranceLenderPool.sol";
+
+contract SideEntranceLenderPoolTest is Test {
+    SideEntranceLenderPool pool;
+    Exploiter exploiter;
+
+    uint256 constant ETHER_IN_POOL = 1 ether;
+
+    function setUp() public {
+        pool = new SideEntranceLenderPool();
+        pool.deposit{value: ETHER_IN_POOL}();
+
+        exploiter = new Exploiter(pool);
+    }
+
+    function test_setup() public view {
+        assert(address(pool).balance == ETHER_IN_POOL);
+    }
+
+    function test_attack() public {
+        // Put your solution here
+
+        _checkSolved();
+    }
+
+    function _checkSolved() internal {
+        assertEq(address(pool).balance, 0);
+        assertGt(address(this).balance, ETHER_IN_POOL);
+    }
+}
