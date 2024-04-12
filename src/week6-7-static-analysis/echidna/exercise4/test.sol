0f6fad89e02f4a173248563ec4c92f880b9b7781
walker <walker@walkPro>
2024-04-11 20:07:44 -0600

    Echidna4 discovers bug

---
+++ src/week6-7-static-analysis/echidna/exercise4/test.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./token.sol";
+
+/// @dev Run the template with
+///      ```
+///      solc-select use 0.8.0
+///      echidna program-analysis/echidna/exercises/exercise4/template.sol --contract TestToken --test-mode assertion
+///      ```
+///      or by providing a config
+///      ```
+///      echidna program-analysis/echidna/exercises/exercise4/template.sol --contract TestToken --config program-analysis/echidna/exercises/exercise4/config.yaml
+///      ```
+contract TestToken is Token {
+    function transfer(address to, uint256 value) public override {
+        // TODO: include `assert(condition)` statements that
+        // detect a breaking invariant on a transfer.
+        // Hint: you may use the following to wrap the original function.
+        uint256 fromBalanceBefore = balances[msg.sender];
+        uint256 toBalanceBefore = balances[to];
+        super.transfer(to, value);
+        uint256 fromBalanceAfter = balances[msg.sender];
+        uint256 toBalanceAfter = balances[to];
+        assert(fromBalanceAfter <= fromBalanceBefore);
+        assert(toBalanceAfter >= toBalanceBefore);
+    }
+}
