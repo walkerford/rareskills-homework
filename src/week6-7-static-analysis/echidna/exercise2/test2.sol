0f1c2a932943555f3fb0a8cf781bddd0a6f30217
Walker Ford <walker.ford@protocol.ai>
2024-04-17 00:33:15 -0600

    Refactored echidna exercise2

--- src/week6-7-static-analysis/echidna/exercise2/test2.sol
+++ src/week6-7-static-analysis/echidna/exercise2/test2.sol
@@ -2,13 +2,26 @@
 pragma solidity ^0.8.0;
 
 import "./token.sol";
+import "./token-fixed.sol" as tf;
 
 /// @dev Run the template with
 ///      ```
 ///      solc-select use 0.8.0
 ///      echidna program-analysis/echidna/exercises/exercise2/template.sol
 ///      ```
-contract TestToken2 is Token {
+contract TestToken is Token {
+    constructor() {
+        pause(); // pause the contract
+        owner = address(0); // lose ownership
+    }
+
+    function echidna_cannot_be_unpause() public view returns (bool) {
+        // TODO: add the property
+        return (paused() == true);
+    }
+}
+
+contract TestTokenFixed is tf.Token {
     constructor() {
         pause(); // pause the contract
         owner = address(0); // lose ownership
