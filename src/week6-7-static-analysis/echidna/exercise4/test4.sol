510256c6775e1f5f04d965ac774d3dc1e06272fc
Walker Ford <walker.ford@protocol.ai>
2024-04-17 00:33:52 -0600

    Refactored echidna exercise4

--- src/week6-7-static-analysis/echidna/exercise4/test4.sol
+++ src/week6-7-static-analysis/echidna/exercise4/test4.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "./token.sol";
+import "./token4.sol";
+import "./token4-fixed.sol" as tf;
 
 /// @dev Run the template with
 ///      ```
@@ -13,6 +14,21 @@ import "./token.sol";
 ///      echidna program-analysis/echidna/exercises/exercise4/template.sol --contract TestToken --config program-analysis/echidna/exercises/exercise4/config.yaml
 ///      ```
 contract TestToken is Token {
+    function transfer(address to, uint256 value) public override {
+        // TODO: include `assert(condition)` statements that
+        // detect a breaking invariant on a transfer.
+        // Hint: you may use the following to wrap the original function.
+        uint256 fromBalanceBefore = balances[msg.sender];
+        uint256 toBalanceBefore = balances[to];
+        super.transfer(to, value);
+        uint256 fromBalanceAfter = balances[msg.sender];
+        uint256 toBalanceAfter = balances[to];
+        assert(fromBalanceAfter <= fromBalanceBefore);
+        assert(toBalanceAfter >= toBalanceBefore);
+    }
+}
+
+contract TestTokenFixed is tf.Token {
     function transfer(address to, uint256 value) public override {
         // TODO: include `assert(condition)` statements that
         // detect a breaking invariant on a transfer.
